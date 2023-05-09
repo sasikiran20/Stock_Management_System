6079104c19ee2bd00f8e027400ad551b228d9ec6
Nirmal Ganesh Yarramaneni <nirmalganesh11@gmail.com>
2023-05-09 20:06:53 +0530

    delete all content
    done by nirmal

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import "./stylesheet/main.sass";
-import App from "./components/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
